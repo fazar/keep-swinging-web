46c7c69e3e94a3e15ab62a4b45b1f3c2ca087a53
Michael Julian Albertus <32842826+ogabeezle@users.noreply.github.com>
2026-09-23 15:39:05 +0700

    add ads

---
+++ web/static/adsense.js
@@ -0,0 +1,7 @@
+/** Google AdSense Configuration */
+window.__ADSENSE_CLIENT_ID__ = 'ca-pub-6796806853565782';
+window.__ADSENSE_SLOTS__ = {
+  home: { id: '1007117379', format: 'horizontal' },
+  session_mid: { id: '4442876068', format: 'vertical' },
+  session_bottom: { id: '5862014927', format: 'rectangle' },
+};
